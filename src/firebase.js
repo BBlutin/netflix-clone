25bbf56e950003ece265f7e6d7ff09dc9beaa404
Thomas Fritschy <thomas.fritschy@gmail.com>
2021-06-01 19:07:42 +0200

    Initial Push

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCIdEBQqQ-52SKwTFVmZJJxBxmHAu8LMM4",
+    authDomain: "netflix-clone-30da9.firebaseapp.com",
+    projectId: "netflix-clone-30da9",
+    storageBucket: "netflix-clone-30da9.appspot.com",
+    messagingSenderId: "7327000980",
+    appId: "1:7327000980:web:240d2af9f9e1f449fa4781"
+  };
